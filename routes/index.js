a3636c492928e90c256136c9830c3ad1869168ca
Calvin Li <calvinli@mit.edu>
2015-09-05 00:10:12 -0400

    changed routes for jotto and minesweeper

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('index');
+  res.render('index.ejs');
+});
+
+router.get('/jotto', function(req, res) {
+    res.render('jotto');
+});
+
+router.get('/minesweeper', function(req, res) {
+    res.render('minesweeper');
 });
 
 module.exports = router;
